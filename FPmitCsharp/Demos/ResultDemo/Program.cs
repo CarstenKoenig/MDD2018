449b8c0d5b0aa6884dd83bc7831c7efd02e603f8
Carsten Koenig <Carsten@gettingsharper.de>
2018-04-04 07:21:03 +0200

    C# Demoprojekt angelegt

---
+++ FPmitCsharp/Demos/ResultDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResultDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
