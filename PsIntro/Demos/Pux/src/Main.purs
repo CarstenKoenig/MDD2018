e70bb62f56bb0b5760505ba484ecd6855e23cb69
Carsten Koenig <Carsten@gettingsharper.de>
2018-04-05 15:24:02 +0200

    PUX - BlackDice Beispiel

--- PsIntro/Demos/Pux/src/Main.purs
+++ PsIntro/Demos/Pux/src/Main.purs
@@ -1,23 +1,62 @@
 module Main where
 
 import Prelude hiding (div)
+
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Random (RANDOM, randomInt)
+import Data.Array (null)
+import Data.Foldable (sum)
+import Data.Maybe (Maybe(..))
+import Data.String (joinWith)
 import Pux (CoreEffects, EffModel, start)
 import Pux.DOM.Events (onClick)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (renderToDOM)
-import Text.Smolder.HTML (button, div, span)
+import Text.Smolder.HTML (button, div, p, span, h1)
 import Text.Smolder.Markup (text, (#!))
 
-data Event = Increment | Decrement
+data Event 
+  = Reset 
+  | Mehr
+  | Wurf Int
 
-type State = Int
+type State = 
+  { gameOver :: Boolean
+  , wuerfe   :: Array Int
+  }
+
+
+initial :: State
+initial = { gameOver: false
+          , wuerfe: []
+          }
+
+punkte :: State -> Int
+punkte = sum <<< _.wuerfe          
+
+
+addWurf :: Int -> State -> State
+addWurf n state =
+  if state.gameOver then
+    state
+  else
+    checkGameOver $ state { wuerfe = state.wuerfe <> [n] }
+
+checkGameOver :: State -> State
+checkGameOver state =
+  if punkte state >= 21 then
+    state { gameOver = true }
+  else
+    state
+
+type AppEffects = ( random :: RANDOM )
 
 -- | Start and render the app
-main :: ∀ fx. Eff (CoreEffects fx) Unit
+main :: Eff (CoreEffects AppEffects) Unit
 main = do
   app <- start
-    { initialState: 0
+    { initialState: initial
     , view
     , foldp: update
     , inputs: []
@@ -27,15 +66,44 @@ main = do
 
 
 -- | Return a new state (and effects) from each event
-update :: ∀ fx. Event -> State -> EffModel State Event fx
-update Increment n = { state: n + 1, effects: [] }
-update Decrement n = { state: n - 1, effects: [] }
+update :: Event -> State -> EffModel State Event AppEffects
+update Reset curState = 
+  { state: initial
+  , effects: [] 
+  }
+update (Wurf n) curState = 
+  { state: addWurf n curState
+  , effects: [] 
+  }
+update Mehr curState = 
+  { state: curState
+  , effects: 
+    [ do
+      w <- Wurf <$> liftEff (randomInt 1 6)
+      pure $ Just w
+    ] 
+  }
 
 
 -- | Return markup from the state
 view :: State -> HTML Event
-view count =
+view state = do
+  h1 $ text "Black-Dice"
   div do
-    button #! onClick (const Increment) $ text "++"
-    span $ text ("  " <> show count <> "  ")
-    button #! onClick (const Decrement) $ text "--"
+    viewWuerfe
+    viewPunkte
+    span $ do
+      button #! onClick (const Mehr) $ text "mehr"
+      button #! onClick (const Reset) $ text "Reset"
+  where
+    viewWuerfe =
+      if null state.wuerfe then
+        p $ text "keien Würfe"
+      else
+        p $ text (joinWith ", " $ show <$> state.wuerfe)
+    viewPunkte =
+      if state.gameOver then
+        p $ text ("GAME OVER - " <> show (punkte state))
+      else
+        p $ text ("Punkte: " <> show (punkte state))
+        
